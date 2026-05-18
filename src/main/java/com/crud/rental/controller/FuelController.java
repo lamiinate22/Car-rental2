cf7a924359750805ce5fb4767fb34129e166aacf
lamiinate22 <maciej.kepa1337@@gmail.com>
2026-05-18 16:46:05 +0200

    add global acces to localhost

--- src/main/java/com/crud/rental/controller/FuelController.java
+++ src/main/java/com/crud/rental/controller/FuelController.java
@@ -4,13 +4,12 @@ import com.crud.rental.domain.FuelPriceDto;
 import com.crud.rental.service.FuelUsageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/fuels")
 @RequiredArgsConstructor
