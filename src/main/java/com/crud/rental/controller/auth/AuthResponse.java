8a79df12f5ed202be8de0697f4b11ff9e57d7946
lamiinate22 <maciej.kepa1337@@gmail.com>
2026-05-28 16:47:27 +0200

    added token response with external data

--- src/main/java/com/crud/rental/controller/auth/AuthResponse.java
+++ src/main/java/com/crud/rental/controller/auth/AuthResponse.java
@@ -1,11 +1,15 @@
 package com.crud.rental.controller.auth;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class AuthResponse {
+    @JsonProperty("token")
     private String token;
+    @JsonProperty("email")
     private String email;
+    @JsonProperty("admin")
     private boolean admin;
 }
